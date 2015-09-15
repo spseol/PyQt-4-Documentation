f18ed250feb2c0f7e337eb087b3079c3a53a977b
SonyPony <sirarseus@gmail.com>
2015-09-15 22:01:48 +0200

    Update canvas-level_1.py

--- Painting/canvas-level_1.py
+++ Painting/canvas-level_1.py
@@ -1,21 +1,21 @@
 import sys
 from PyQt4.QtGui import QWidget, QPainter, QPen, QApplication, QColor
 
-#naše upravená třída, kde si čmrkáme
+# naše upravená třída, kde si čmrkáme
 class MainWindow(QWidget):
     def __init__(self):
         super(MainWindow, self).__init__()
 
-    #v C++ virtuální metoda, paint_event pouze obsahuje oblast obnovení
+    # v C++ virtuální metoda, paint_event pouze obsahuje oblast obnovení
     def paintEvent(self, paint_event):
-        #nastavíme pero na červenou barvu se šířkou 10px
+        # nastavíme pero na červenou barvu se šířkou 3px
         pen = QPen(QColor("red"))
         pen.setWidth(3)
 
         painter = QPainter(self)
         painter.setPen(pen)
 
-        #drawLine(x1, y1, x2, y2)
+        # drawLine(x1, y1, x2, y2)
         painter.drawLine(0, 0, 100, 100)
 
 app = QApplication(sys.argv)
